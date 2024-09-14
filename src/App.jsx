cc033d122ef4c706d8932ce974e5f1f8202d7b26
abhinav-gif <65060617+abhinav-gif@users.noreply.github.com>
2024-09-14 23:01:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@ import MemoryGame from "./pages/gamepage/MemoryGame";
 import HomePage from "./pages/homepage/HomePage";
 import Auth from "./pages/accesspage/Auth";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-const apiEndpoint = "https://memory-game-shram.vercel.app";
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path={`${apiEndpoint}/`} element={<HomePage />} />
-          <Route path={`${apiEndpoint}/game/:mode`} element={<MemoryGame />} />
-          <Route
-            path={`${apiEndpoint}/authentication/:user`}
-            element={<Auth />}
-          />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/game/:mode" element={<MemoryGame />} />
+          <Route path="/authentication/:user" element={<Auth />} />
         </Routes>
       </div>
     </Router>
