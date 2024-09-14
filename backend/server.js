4b226e783393aa95f0c7da549394343cf386164a
abhinav-gif <65060617+abhinav-gif@users.noreply.github.com>
2024-09-15 00:36:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["http://localhost:3000"],
+        origin: ["https://memory-game-sooty-ten.vercel.app"],
         credentials: true
     }
 ));
